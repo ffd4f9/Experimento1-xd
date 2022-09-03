ef2940825f03d556f03e3393c4166b9ed5235c02
Wagner Fuentes Quintana <ich.bin.wagner@gmail.com>
2022-09-03 11:11:52 -0600

    version2

--- Persona.py
+++ Persona.py
@@ -5,4 +5,12 @@ class Persona:
         self.__estatura = estatura
 
     def mostrar(self):
-        print(f"nombre: {self.__nombre}\nedad: {self.__edad}\nestatura: {self.__estatura}")
+        print(f"Nombre: {self.__nombre}\nEdad: {self.__edad}\nEstatura: {self.__estatura}")
+class Empleado(Persona):
+    def __init__(self,nombre:str,edad:int,estatura:float,especialidad:str,tiempoActivo:int):
+        super().__init__(nombre,edad,estatura)
+        self.__especialidad = especialidad
+        self.__tiempoActivo = tiempoActivo
+    def mostrar(self):
+        super().mostrar()
+        print(f"Especialidad: {self.__especialidad}\nTiempo activo: {self.__tiempoActivo} periodos")
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from Persona import Persona
+from Persona import Persona,Empleado
 if __name__ == "__main__":
     personas = []
     personas.append(Persona("Wagner",24,1.75))
-    personas.append(Persona("Luis",22,1.75))
+    personas.append(Empleado("Luis",22,1.75,"Profesor",3))
     personas.append(Persona("Bryan",29,1.75))
-    personas.append(Persona("Dennis",24,1.70))
+    personas.append(Empleado("Dennis",24,1.70,"Biologo",1))
     personas.append(Persona("Alejandro",22,1.75))
-    personas.append(Persona("Monica",21,1.60))
+    personas.append(Empleado("Monica",21,1.60,"Trabajadora Social",2))
     print("**********")
     for persona in personas:
         persona.mostrar()
